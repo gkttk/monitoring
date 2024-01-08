9265666ebc855ff0f4771ac525abbb2d911db6f3
Kirill <gkttk@mail.ru>
2024-01-08 16:04:40 +0300

    Add createNotification Mutation

--- src/main/java/com/gkttk/monitoring/controllers/graphql/NotificationGraphQLController.java
+++ src/main/java/com/gkttk/monitoring/controllers/graphql/NotificationGraphQLController.java
@@ -6,6 +6,7 @@ import com.gkttk.monitoring.services.NotificationService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +21,11 @@ public class NotificationGraphQLController {
   @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
   public List<NotificationDto> notifications(@Argument(name = "filter") NotificationFilter filter) {
     return service.getAll(filter);
+  }
+
+  @MutationMapping
+  @PreAuthorize("hasRole('ADMIN')")
+  public NotificationDto createNotification(@Argument NotificationDto notification) {
+    return service.create(notification);
   }
 }
